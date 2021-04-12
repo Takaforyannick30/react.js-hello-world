8d188c02ba0f39722b5f732f80b5fd8e4bcaab6e
Takaforyannick30 <takaforyannick30@gmail.com>
2021-04-13 01:30:52 +0200

    Transfered Header Component to Header.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+
 
 class App extends Component{
   render() {
@@ -14,15 +16,6 @@ class App extends Component{
   }
   }
 
-  class Header extends Component {
-    render() {
-      return(
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-      );
-    }
-  }
 
   class Intro extends Component {
     render() {
--- src/Header.js
+++ src/Header.js
@@ -1,1 +1,17 @@
 import React, {Component} from 'react';
+
+
+class Header extends Component {
+    render() {
+      return(
+        <div className="App-header">
+        {
+        //   <img src={logo} className="App-logo" alt="logo" />
+          
+        }
+        </div>
+      );
+    }
+}
+
+export default Header;
