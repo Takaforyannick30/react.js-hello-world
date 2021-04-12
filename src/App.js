aace41ccfdb5e44cfe9052f7353c4c680166689d
Takaforyannick30 <takaforyannick30@gmail.com>
2021-04-13 01:10:42 +0200

    Created seperate class component for Header and Intro in app.js

--- src/App.js
+++ src/App.js
@@ -6,25 +6,42 @@ class App extends Component{
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
+        <Header />
+       
+        <Intro />
       </div>
     );
   }
+  }
 
+  class Header extends Component {
+    render() {
+      return(
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+      );
+    }
+  }
+
+  class Intro extends Component {
+    render() {
+      return(
+        <div>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </div>
+      );
+    }
   }
  
 
