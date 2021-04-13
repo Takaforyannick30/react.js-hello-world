e9911f8682c48c8df1f0c8261aadd425383041e6
Takaforyannick30 <takaforyannick30@gmail.com>
2021-04-13 03:54:09 +0200

    Created Parent and Cars Components in Header.js

--- src/Header.js
+++ src/Header.js
@@ -18,10 +18,34 @@ class Header extends Component {
       return(
         <div className="App">
             <h2>Just some sample data:{this.yourname}</h2>
+            <Parent />
        
         </div>
+        
       );
     }
 }
+
+class Parent extends Component {
+    render() {
+        return(
+            <div>
+                <h1>I am the parent</h1>
+                <Cars />
+            </div>
+        );
+    }
+}
+
+class Cars extends Component{
+    render(){
+        return(
+            <div>
+                I am from cars component
+            </div>
+        );
+    }
+}
+
 
 export default Header;
